85a070009e188616a37c92496e98e74c373946cb
erpnextdeveloper2 <56186849+erpnextdeveloper2@users.noreply.github.com>
2020-03-09 01:48:49 +0530

    added new openapi aws in integrated

--- src/views/FinanceTabs/OpenApi.vue
+++ src/views/FinanceTabs/OpenApi.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <!-- <p>{{getOpenApi}}</p> -->
-    <open-api v-if="getOpenApi" :api="getOpenApi" :query-params="queryParams" :headers="headers"></open-api>
+    <open-api
+      v-if="getOpenApi"
+      :api="getOpenApi"
+      :query-params="queryParams"
+      operation="create_aws_instances_create_post"
+      :headers="headers"
+    ></open-api>
   </div>
 </template>
 
@@ -24,7 +29,7 @@ export default {
       // operationId: "addPet"
       // operationId: "addPet"
       // operationId: "findPetsByStatus"
-      operationId: "add_contact_check_add_contact_post"
+      operationId: "create_aws_instances_create_post"
     },
     headers: {
       api_key: "my_api_key"
